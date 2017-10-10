2cdd8e60e499dc9b8f0e4b42c1877f0069de6d78
waylybaye <havelove@gmail.com>
2017-10-10 23:25:13 +0800

    import PropTypes from prop-types

--- WKWebView.ios.js
+++ WKWebView.ios.js
@@ -1,8 +1,9 @@
 'use strict';
 
-import React, {
-  PropTypes
-} from 'react';
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
 import ReactNative, {
   requireNativeComponent,
   EdgeInsetsPropType,
